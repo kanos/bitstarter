211704ea13c0967a72dd248fdd424d09451df2bb
J.Carlos <jcarlolb@gmail.com>
2013-07-29 19:22:20 -0500

    Updated web.js

--- web.js
+++ web.js
@@ -1,11 +1,15 @@
+var fs=require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    fs.readFile("index.html", function (err,data) {
+	if (err) throw err;
+	response.send(data.toString());
+    });
 });
-
+   
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
